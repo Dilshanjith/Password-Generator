10e5ee116e6cc140c9e8384d8958b25c67d153ea
Dilshan samarasinghe <113366721+Dilshanjith@users.noreply.github.com>
2025-01-14 11:29:42 +0530

    update css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-
+import React,{useState} from 'react';
 import './App.css';
+import{
+  numbers,
+  upperCaseLetters,
+  lowerCaseLetters,
+  specialCharacters,
+}from "./characters"
 
 function App() {
+  const [password,setPassword]=useState('')
+  const [passwordlength, setPasswordlength] = useState(20)
+  const[includeUppercase,setIncludeUppercase] = useState(false)
+  const[includeLowercase,setIncludeLowercase] = useState(false)
+  const[includeNumbers,setIncludeNumbers] = useState(false)
+  const[includeSymbols,setIncludeSymbbols] = useState(false)
+
+  const handleGeneratePassword =(e) => {
+    let characterList = ''
+
+    if(includeLowercase){
+      characterList = characterList + lowerCaseLetters
+    }
+
+    if(includeUppercase){
+      characterList = characterList + upperCaseLetters
+    }
+
+    if(includeNumbers){
+      characterList = characterList + numbers
+    }
+
+    if(includeSymbols){
+      characterList = characterList + specialCharacters
+    }
+
+    setPassword(characterList)
+  }
+
   return (<div className="App">
     <div className="container">
       <div className="generator">
         <h2 className="generator_heder">
           password Generator</h2>
         <div className="generator-password">
-          <h3>password</h3>
+          <h3>{password}</h3>
           <button className="copy_btn">
           <i className='far fa-clipboard'></i>
           </button>
         </div>
 
         <div className="form-group">
-          <lable htmlFor="password-strength">password Strength</lable>
+          <lable htmlFor="password-strength">password length</lable>
           <input 
+          defaultValue={passwordlength}
+          onChange={(e) => setPasswordlength(e.target.value)}
           type="number" 
           id="password-strength" 
           name="password-strength"
@@ -28,6 +65,9 @@ function App() {
         <div className="form-group">
           <lable htmlFor="uppercase-letters">Include Uppercase Letters</lable>
           <input 
+          
+          checked ={includeUppercase}
+          onChange={(e) => setIncludeUppercase(e.target.checked)}
           type="checkbox" 
           id="uppercase-letters" 
           name="uppercase-letters"
@@ -38,7 +78,9 @@ function App() {
 
         <div className="form-group">
           <lable htmlFor="lowercase-letters">Include Lowercase Letters</lable>
-          <input 
+          <input
+          checked ={includeLowercase} 
+          onChange={(e) => setIncludeLowercase(e.target.checked)}
           type="checkbox" 
           id="lowercase-letters" 
           name="lowercase-letters"
@@ -50,6 +92,8 @@ function App() {
         <div className="form-group">
           <lable htmlFor="include-numbers">Include Number</lable>
           <input 
+          checked ={includeNumbers}
+          onChange={(e) => setIncludeNumbers(e.target.checked)}
           type="checkbox" 
           id="include-numbers" 
           name="include-numbers"
@@ -59,13 +103,15 @@ function App() {
         <div className="form-group">
           <lable htmlFor="include-symbols">Include Symbols</lable>
           <input 
+          checked ={includeSymbols}
+          onChange={(e) => setIncludeSymbbols(e.target.checked)}
           type="checkbox" 
           id="include-symbols" 
           name="include-symbols"
           />
         </div>
 
-        <button className="generator_btn">Generate Button</button>
+        <button onClick={handleGeneratePassword} className="generator_btn">Generate Password</button>
 
       </div>
     </div>
